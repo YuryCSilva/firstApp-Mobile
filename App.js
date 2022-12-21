9efae5a66d0f312f91377a253fe0a51ad8cf1c21
Yury Silva <ysilva@marlin.com>
2022-12-20 23:14:12 -0300

    refactor: Add home component in App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
+import Home from "./src/screens/Home";
 
 export default function App(){
   return(
     <SafeAreaView>
-      <Text>Olá Mundo</Text>
+      <Home />
     </SafeAreaView>
   )
 }
